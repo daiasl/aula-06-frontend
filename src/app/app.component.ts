bb2d5e37829df09a55bc464d1c5c6013dc03e76b
daiasl <55289150+daiasl@users.noreply.github.com>
2019-10-26 16:10:49 -0300

    Aula 06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dev-na-pratica-frontend-basico';
+  
+  marcas: any=[];
+  veiculos: any=[];
+  modelo: any=[];
+  
+  constructor (protected readonly http: HttpClient){
+    this.main()
+  }
+  async main(){
+    //promise
+    this.marcas= await this.http
+      .get('http://fipeapi.appspot.com/api/1/carros/marcas.json')
+      .toPromise();
+    console.log(this.marcas, 'Marcas');
+
+    //filtra pela marca.
+    const volkswagen = this.marcas.filter((mar: any)=> mar.name === 'VOLKSWAGEN')[0];
+    console.log(volkswagen);
+
+    this.veiculos = await this.http
+      .get(`http://fipeapi.appspot.com/api/1/carros/veiculos/${volkswagen.id}.json`)
+      .toPromise()
+    console.log(this.veiculos, 'Veiculos');
+
+    this.modelo =this.veiculos.filter((vei: any)=> vei.name === 'AMAROK Trendline CD 2.0 TDI 4X4 Dies Aut');
+    console.log (this.modelo, 'Modelo');
+
+    
+    
+
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { FeatureModule } from './feature/feature.module';
+import { CardModule } from "primeng/card";
+import { AppRoutingModule } from './app.routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    FeatureModule,
+    CardModule,
+    AppRoutingModule,
+    HttpClientModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
